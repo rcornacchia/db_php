95e1875e224bee3f827645162c4b389cd6521e09
Rob Cornacchia <bobbycornacchia@gmail.com>
2015-06-23 10:06:28 -0400

    Initial commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+	// if (!empty($_SERVER['HTTPS']) && ('on' == $_SERVER['HTTPS'])) {
+	// 	$uri = 'https://';
+	// } else {
+	// 	$uri = 'http://';
+	// }
+	// $uri .= $_SERVER['HTTP_HOST'];
+	// header('Location: '.$uri.'/xampp/');
+	// exit;
+
+	// echo "My first PHP script";
+
+	$x = 1;
+	$y = $x * 5;
+	echo $y;
+
+
+?>
