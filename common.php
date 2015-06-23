eee6a7e1d5a69394e90202f4b86d25905714c1b8
Rob Cornacchia <bobbycornacchia@gmail.com>
2015-06-23 14:59:17 -0400

    polishing

--- common.php
+++ common.php
@@ -1,14 +1,13 @@
 <?php
 	// display errors/debugging
 	ini_set('display_errors', 'On');
-
 	// connection to database
 	// don't forget to close database connection using mysqli($conn)
 	$dburl = "cs4111.cfziojgqxy1i.us-east-1.rds.amazonaws.com";
 	$dbuser = "rlc2160";
 	$dbpassword = "t3chmark";
 	$dbname = "cs4111";
-	$conn = mysql_connect($dburl, $dbuser, $dbpassword, $dbname);
+	$conn = mysqli_connect($dburl, $dbuser, $dbpassword, $dbname);
 	if(!$conn){
 		die ("It didn't work!");
 	}
--- login.php
+++ login.php
@@ -1,31 +1,60 @@
 <?php
 	require_once "common.php";
-
 	$email = $_REQUEST["email"];
-
 	// adding check for user submits blank textbox
-
-
-	$query = sprintf("SELECT * 
-						FROM Users, Teachers
-						WHERE Users.uemail = '%s' OR Teachers.temail = '%s'",
-						mysql_real_escape_string($email),
-						mysql_real_escape_string($email));
-	// mysql_real_escape_string
-
-	// Perform Query
-	$result = mysql_query($query);
-
-
+	$query = sprintf("SELECT * FROM Users WHERE Users.uemail = '%s'",
+                        mysqli_real_escape_string($conn,$email));
+    
+    // Perform Query
+    $result = mysqli_query($conn,$query);
 	if(!$result) {
 		$message = 'Invalid query: ' . mysql_error() . "\n";
 		$message .= 'Whole query: ' . $query;
 		die($message);
 	}
-
-	// Use result
-	while($row = mysql_fetch_assoc($result)) {
-		print_r($row);
-	}
-
+    else{
+//        print($query);
+    }
+    
+    $row_cnt = $result -> num_rows;
+    
+    if($row_cnt == 1){
+        print('user is student');
+        $_SESSION["email"] = $email;
+        //redirect to correct webpage
+        header("Location: localhost/studentHome.php"); /* Redirect browser */
+        exit();
+    }
+    else{
+       /* start query on teacher table */
+        $query = sprintf("SELECT * FROM Teachers WHERE Teachers.temail = '%s'",
+                         mysqli_real_escape_string($conn,$email));
+        
+        
+    
+        if(!$result) {
+            $message = 'Invalid query: ' . mysql_error() . "\n";
+            $message .= 'Whole query: ' . $query;
+            die($message);
+        }
+        else{
+//            print($query);
+        }
+        
+        // Perform Query
+        $result = mysqli_query($conn,$query);
+        $row_cnt = $result -> num_rows;
+        
+        if($row_cnt == 1){
+            print('user is teacher');
+            $_SESSION["email"] = $email;
+            //redirect to correct webpage
+            header("Location: localhost/teacherHome.php"); /* Redirect browser */
+            exit();
+        }
+        else{
+            die('email not in DB');
+        }
+    }
+    
 ?>
