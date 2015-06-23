a6ad4f1c3f87b8fa30d8edfb67d4cf168d7b679e
Rob Cornacchia <bobbycornacchia@gmail.com>
2015-06-23 14:58:45 -0400

    Added teacher and student home pages

---
+++ teacherhome.php
@@ -0,0 +1,25 @@
+<?php
+
+	
+
+?>
+
+<!DOCTYPE HTML>
+<html> 
+<body>
+
+	<h2>Add a Student you've taught</h2>
+	<form action="addStudent.php" method="get">
+		Name: <input type="text" name="studentName"><br>
+		Name: <input type="text" name="studentEmail"><br>
+		<input type="submit" value="Add">
+	</form>
+
+	<h2>Add a School where you've taught</h2>
+	<form action="addSchool.php" method="get">
+		Name: <input type="text" name="schoolName"><br>
+		<input type="submit" value="Add">
+	</form>
+
+</body>
+</html>
