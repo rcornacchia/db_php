4dd32aa3bc6ce5b17e547a904d00ea39c8cce4de
Roy Hermann <royhermann@dyn-160-39-202-113.dyn.columbia.edu>
2015-06-23 14:52:23 -0400

    added redirecting functioanltiy afte rlogin

--- login.php
+++ login.php
@@ -6,24 +6,63 @@
 	// adding check for user submits blank textbox
 
 
-	$query = sprintf("SELECT * FROM Users, Teachers WHERE Users.uemail = '%s' OR Teachers.temail = '%s'",
-                        mysqli_real_escape_string($conn,$email),
-						mysqli_real_escape_string($conn,$email));
-	// mysql_real_escape_string
-
-	// Perform Query
-	$result = mysqli_query($conn,$query);
+	$query = sprintf("SELECT * FROM Users WHERE Users.uemail = '%s'",
+                        mysqli_real_escape_string($conn,$email));
+    
 
 
+    // Perform Query
+    $result = mysqli_query($conn,$query);
 	if(!$result) {
 		$message = 'Invalid query: ' . mysql_error() . "\n";
 		$message .= 'Whole query: ' . $query;
 		die($message);
 	}
+    else{
+//        print($query);
+    }
+    
 
-	// Use result
-	while($row = mysqli_fetch_assoc($result)) {
-		print_r($row);
-	}
+    $row_cnt = $result -> num_rows;
+    
+    if($row_cnt == 1){
+        print('user is student');
+        $_SESSION["email"] = $email;
+        //redirect to correct webpage
+        header("Location: localhost/studentHome.php"); /* Redirect browser */
+        exit();
+    }
+    else{
+       /* start query on teacher table */
+        $query = sprintf("SELECT * FROM Teachers WHERE Teachers.temail = '%s'",
+                         mysqli_real_escape_string($conn,$email));
+        
+        
+    
+        if(!$result) {
+            $message = 'Invalid query: ' . mysql_error() . "\n";
+            $message .= 'Whole query: ' . $query;
+            die($message);
+        }
+        else{
+//            print($query);
+        }
+        
+        // Perform Query
+        $result = mysqli_query($conn,$query);
+        $row_cnt = $result -> num_rows;
+        
+        if($row_cnt == 1){
+            print('user is teacher');
+            $_SESSION["email"] = $email;
+            //redirect to correct webpage
+            header("Location: localhost/teacherHome.php"); /* Redirect browser */
+            exit();
+        }
+        else{
+            die('email not in DB');
+        }
+    }
+    
 
 ?>
