5f89928d0059c01d352b744c9416bbc3c47d246f
Rob Cornacchia <bobbycornacchia@gmail.com>
2015-06-23 14:05:04 -0400

    added login page

--- common.php
+++ common.php
@@ -4,9 +4,12 @@
 
 	// connection to database
 	// don't forget to close database connection using mysqli($conn)
-	$dburl = "cs4111.cfziojgqxy1i.us-east-1.rds.amazonaws.com:3306";
+	$dburl = "cs4111.cfziojgqxy1i.us-east-1.rds.amazonaws.com";
 	$dbuser = "rlc2160";
+	$dbpassword = "t3chmark";
 	$dbname = "cs4111";
-	$conn = mysqli_connect($dburl, $dbuser, $dbpassword, $dbname);
-
+	$conn = mysql_connect($dburl, $dbuser, $dbpassword, $dbname);
+	if(!$conn){
+		die ("It didn't work!");
+	}
 ?>
--- index.php
+++ index.php
@@ -10,11 +10,38 @@
 
 	// echo "My first PHP script";
 
-	$x = 0;
-	session_start(); 
-	$_SESSION['views'] = $x + 1; // store session data
-	echo "Pageviews = ". $_SESSION['views']; //retrieve data
+	// connect to database
+	require_once "common.php";
+
+
+	// $x = 0;
+	// session_start(); 
+	// $_SESSION['views'] = $x + 1; // store session data
+	// echo "Pageviews = ". $_SESSION['views']; //retrieve data
 
 
 
 ?>
+
+<!DOCTYPE HTML>
+<html> 
+<body>
+
+	<form action="login.php" method="get">
+	<!-- Name: <input type="text" name="name"><br> -->
+	E-mail: <input type="text" name="email"><br>
+	<input type="submit" value="Log In">
+	</form>
+
+	<h2>OR</h2>
+
+	<form action="signup.php" method="get">
+	Name: <input type="text" name="name"><br>
+	E-mail: <input type="text" name="email"><br>
+	School: <input type="text" name="school"><br>
+	<input type="submit" value="Register">
+	</form>
+
+
+</body>
+</html>
