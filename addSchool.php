626a2bd2e2257c0ff74195b801a1536feea12081
Rob Cornacchia <bobbycornacchia@gmail.com>
2015-06-24 18:24:46 -0400

    addSchool.php works

--- addSchool.php
+++ addSchool.php
@@ -2,25 +2,54 @@
 	require_once "common.php";
 	
 	// get email from login page
-	$email = $_SESSION["email"];
+	$temail = $_SESSION["email"];
 
 	// get school name from previous page
 	$sname = $_REQUEST['schoolName'];
 
 	// check to see if the teacher has already added school
-	
-
-	// perform query
-	$query = sprintf("SELECT Teachers.tname FROM Teachers WHERE Teachers.temail = '%s'", $email);
+	$query = sprintf("SELECT Schools.sname FROM Schools WHERE Schools.sname = '%s'", $sname);
 	$result = mysqli_query($conn, $query);
+
+	//check to make sure the result was returned
 	if(!$result) {
-		$message = 'Invalid query: ' . mysql_error() . "\n";
-		$message .= 'Whole query: ' . $query;
-		die($message);
+		$message = 'problem with select * from into question type query ';
+		$message = $message . 'Invalid query: ' . mysql_error() . "\n";
+	    die($message);
+	} else {
+		// if school doesn't exist, add it to Schools
+		$numRows = $result -> num_rows;
+		if($numRows == 0) {
+
+			$query = sprintf("INSERT INTO Schools(sname)
+			VALUES ('%s')", $sname);
+			$result = mysqli_query($conn, $query);
+			if($result){
+				print "We've never seen that school before. Don't worry, we'll add it to the list." . "<br>";
+			}
+		} 
+	// either way, add school to Teaches_At
+
+		// get teacher tid
+		$query = sprintf("SELECT Teachers.tid FROM Teachers WHERE Teachers.temail = '%s'", $temail);
+		$result = mysqli_query($conn, $query);
+		$row = mysqli_fetch_assoc($result);
+		$tid = $row['tid']; 
+
+		// get School sid
+		$query = sprintf("SELECT Schools.sid FROM Schools WHERE Schools.sname = '%s'", $sname);
+		$result = mysqli_query($conn, $query);
+		$row = mysqli_fetch_assoc($result);
+		$sid = $row['sid']; 
+
+		// add school to Teaches_At Table
+		$query = sprintf("INSERT INTO Teaches_At(tid, sid)
+		VALUES ('%s', '%s')", $tid, $sid);
+		$result = mysqli_query($conn, $query);
+
+		if($result){
+			// successful query
+			print "School successfully added!";
+		}
 	}
-
-	$row = $result->fetch_assoc();
-
-	print "Welcome " . $row["tname"];
-
 ?>
