d2d21e39feb419c1282d54515dd01bd40e0c2ba6
Roy Hermann <royhermann@dyn-160-39-203-40.dyn.columbia.edu>
2015-06-24 20:59:04 -0400

    students

--- addStudent.php
+++ addStudent.php
@@ -6,6 +6,7 @@
 
 	// get Student's email from previous page
 	$uemail = $_REQUEST["studentEmail"];
+    
 
 	// get the user id from the student email
 	$query = sprintf("SELECT Users.uid FROM Users WHERE Users.uemail = '%s'", $uemail);
@@ -28,35 +29,33 @@
 	// get user id from result
 	$row = mysqli_fetch_assoc($result);
 	$uid = $row['uid'];
+    
+    $query = sprintf("SELECT Teachers.tid FROM Teachers WHERE Teachers.temail = '%s'", $temail);
+    $result = mysqli_query($conn, $query);
+    if(!$result) {
+        $message = 'problem with select Teachers.tid FROM Teachers type query  ';
+        $message = $message . 'Invalid query: ' . mysql_error() . "\n";
+        die($message);
+        
+    }
+    $row = mysqli_fetch_assoc($result);
+    $tid = $row['tid'];
+    
+    $query = sprintf("INSERT INTO Taught_By(uid, tid)
+                     VALUES ('%s', '%s')", $uid, $tid);
+                     $result = mysqli_query($conn, $query);
+    
+     if(!$result) {
+     
+     $message = 'problem with select * FROM Taughty_By WHERE Teaches_At.uid   ';
+     $message = $message . 'Invalid query: ' . mysql_error() . "\n";
+     die($message);
+     
+     
+     }
+                     else{
+                     echo 'worked!';
+                     }
 
-	// query the database to see if the user is already being taught by teacher
-	print $uid;
-	print "<br>";
-	$query = sprintf("SELECT * FROM Teaches_At WHERE Teaches_At.uid = '%s'", $uid);
-	$result = mysqli_query($conn, $query);	
 
-	if(!$result) {
-					// get teacher tid
-			$query = sprintf("SELECT Teachers.tid FROM Teachers WHERE Teachers.temail = '%s'", $temail);
-			$result = mysqli_query($conn, $query);
-			$row = mysqli_fetch_assoc($result);
-			$tid = $row['tid']; 
-			print("$uid" . "<br>");
-			print("$tid" . "<br>");
-			// add student to taught_by table
-			$query = sprintf("INSERT INTO Taught_By(uid, tid)
-			VALUES ('%s', '%s')", $uid, $tid);
-			$result = mysqli_query($conn, $query);
-				// $message = 'problem with select * from into question type query  ';
-		// $message = $message . 'Invalid query: ' . mysql_error() . "\n";
-	 //    die($message);
-
-	}else {
-		$numRows = $result -> num_rows;
-		if($numRows == 0) {
-
-		} else {
-			print "User has already been added";
-		}
-	}
 ?>
